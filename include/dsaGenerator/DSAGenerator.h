75fc36a96133cac052fa3c42456a3b98da0bfeca
Vikram Narayanan <vikram186@gmail.com>
2016-10-18 13:34:04 +0200

    dsaGenerator: Override/implement passName function
    Also use clang-format tool to format the code
    
    Signed-off-by: Vikram Narayanan <vikram186@gmail.com>

--- include/dsaGenerator/DSAGenerator.h
+++ include/dsaGenerator/DSAGenerator.h
@@ -4,33 +4,34 @@
 #ifndef DSAGENERATOR_H
 #define DSAGENERATOR_H
 
-#include "llvm/Pass.h"
-#include "dsa/DataStructure.h"
 #include "dsa/DSGraph.h"
 #include "dsa/DSNode.h"
+#include "dsa/DataStructure.h"
+#include "llvm/Pass.h"
 
 namespace dsa {
 
 using namespace llvm;
-  
-class DSAGenerator: public llvm::ModulePass {
+
+class DSAGenerator : public llvm::ModulePass {
 private:
-BUDataStructures* BU;
+  BUDataStructures *BU;
 
 public:
   static char ID; // Pass identification, replacement for typeid
-  
-  DSAGenerator() : llvm::ModulePass(ID) {}
-  
-  virtual bool runOnModule(llvm::Module& m);
-  
+  std::string passName;
+
+  DSAGenerator() : llvm::ModulePass(ID), BU(NULL), passName("DSAGenerator") {}
+
+  virtual const char *getPassName() const { return passName.c_str(); }
+
+  virtual bool runOnModule(llvm::Module &m);
+
   virtual void getAnalysisUsage(AnalysisUsage &AU) const {
     AU.addRequired<BUDataStructures>();
     AU.setPreservesAll();
   }
 };
-
 }
 
-#endif //DSAGENERATOR_H
-
+#endif // DSAGENERATOR_H
